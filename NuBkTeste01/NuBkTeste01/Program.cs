acb8c4ed4c7b83ec544e76889e6b23ba777d79f2
David Ribeiro <dcaliari@gmail.com>
2022-05-19 09:03:28 -0300

    Adicionando arquivo de configuração e configurando o Serilog para logar as informações em qualquer lugar ou arquivo

--- NuBkTeste01/NuBkTeste01/Program.cs
+++ NuBkTeste01/NuBkTeste01/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
 
 namespace NuBkTeste01
 {
@@ -6,7 +9,25 @@ namespace NuBkTeste01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Log.Logger.Information("Iniciando Aplicação!");
+            
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables();
         }
     }
 }
